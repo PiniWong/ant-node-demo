e017dca564521b61a9d8e837f8469a71719f674e
PiniWong <79297110+PiniWong@users.noreply.github.com>
2021-12-13 17:39:34 +0800

    1

---
+++ plugins/DataBase.ini.js
@@ -0,0 +1,9 @@
+const Database = {}
+
+Database.host = 'localhost'
+Database.user ='root'
+Database.password = '123456'
+Database.port = 3306
+Database.database = 'pay'//数据库名
+
+exports.dbMysql=Database
